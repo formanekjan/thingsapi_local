d934982bbe2531ee329380acd2f6b65c7b56a713
Jan Formanek <formanek@teco.edu>
2019-02-07 11:44:06 +0100

    inital commit

---
+++ esp32_thingsapi.ino
@@ -0,0 +1,27 @@
+#include <WiFi.h>
+#include "wlan_credentials.h"
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  
+
+  Serial.println("Startup_completed");
+ 
+
+  
+  
+
+}
+
+void loop() {
+  
+  
+  delay(3000);
+ 
+
+  
+  
+
+}
