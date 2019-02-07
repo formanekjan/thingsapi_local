96df6900bc603f88fdbf0d718f4df69b04c6033e
Jan Formanek <formanek@teco.edu>
2019-02-07 15:33:26 +0100

    added HTTP request, working

--- esp32_thingsapi.ino
+++ esp32_thingsapi.ino
@@ -1,14 +1,27 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
 #include "wlan_credentials.h"
+
+//HTTPClient http;
+
+void connectToNetwork() {
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Establishing connection to WiFi..");
+  }
+
+  Serial.println("Connected to network");
+  //http.begin("http://jsonplaceholder.typicode.com/comments?id=10");
+
+}
 
 void setup() {
   
   Serial.begin(115200);
-
-  
-
-  Serial.println("Startup_completed");
- 
+  Serial.println("Setup");
+  connectToNetwork();
 
   
   
@@ -18,10 +31,27 @@ void setup() {
 void loop() {
   
   
-  delay(3000);
- 
+    if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
 
-  
-  
+    HTTPClient http;
+
+    http.begin("http://jsonplaceholder.typicode.com/comments?id=10"); //Specify the URL
+    int httpCode = http.GET();                                        //Make the request
+
+    if (httpCode > 0) { //Check for the returning code
+
+        String payload = http.getString();
+        Serial.println(httpCode);
+        Serial.println(payload);
+      }
+
+    else {
+      Serial.println("Error on HTTP request");
+    }
+
+    http.end(); //Free the resources
+  }
+
+  delay(10000);
 
 }
